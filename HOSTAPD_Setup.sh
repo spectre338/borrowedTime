ff2aa79e6250c666d25bebb31727d4a4f8f452fb
W7SVT <W7SVT@icloud.com>
2021-10-16 15:11:56 -0700

    add Docker

--- HOSTAPD_Setup.sh
+++ HOSTAPD_Setup.sh
@@ -30,3 +30,11 @@ curl -sSL https://get.docker.com | sh
 
 
 
+sudo usermod -aG docker ${USER}
+sudo apt-get install libffi-dev libssl-dev
+sudo apt install python3-dev
+sudo apt-get install -y python3 python3-pip
+
+sudo pip3 install docker-compose
+
+sudo systemctl enable docker
