46b73a14e38266bffc9fdd1fa00a8b2a1e875379
Drew <drew@zapatatechnology.com>
2021-10-16 11:08:48 -0700

    Automagic ntp.conf settings

--- GPSD_NTP_Setup.sh
+++ GPSD_NTP_Setup.sh
@@ -31,6 +31,15 @@ sed -i 's:GPSD_OPTIONS="":GPSD_OPTIONS="-n":g' /etc/default/gpsd
 echo dtoverlay=pps-gpio,gpiopin=18 >> /boot/config.txt
 echo pps-gpio >> /etc/modules
 #*******************************************************
+# JIC
+#*******************************************************
+telinit q
+#*******************************************************
+# Enable services
+#*******************************************************
+systemctl enable gpsd
+systemctl enable ntp
+#*******************************************************
 # Comment out the internet time sources
 #*******************************************************
 sed -i 's/pool 0./# pool 0./g' /etc/ntp.conf
@@ -38,12 +47,26 @@ sed -i 's/pool 1./# pool 1./g' /etc/ntp.conf
 sed -i 's/pool 2./# pool 2./g' /etc/ntp.conf
 sed -i 's/pool 3./# pool 3./g' /etc/ntp.conf
 #*******************************************************
-# Enable services
+# Confiure /etc/ntp.conf for SHM2 GPS and PPS
 #*******************************************************
-systemctl enable gpsd
-systemctl enable ntp
+sudo cat << EOF >> /etc/ntp.conf
+#Note that this config works without an internet source.
+pool us.pool.ntp.org iburst noselect
 
-#*******************************************************
-# JIC
-#*******************************************************
-telinit q
+broadcast 172.16.0.255
+
+#PPS Kernel driver
+server 127.127.22.0 minpoll 4 maxpoll 4 true
+fudge 127.127.22.0 flag3 1 refid PPS
+
+# GPS Serial NMEA Driver
+server 127.127.28.0 minpoll 4 maxpoll 4 iburst prefer
+fudge 127.127.28.0 flag1 1 time1 0.250 refid GPS stratum 1
+
+# Shared Host Memory 2 source
+server 127.127.28.2 minpoll 4 maxpoll 4
+fudge 127.127.28.2 flag1 1 refid SHM2
+
+# Fix False tickers
+tos mindist 0.5
+EOF
