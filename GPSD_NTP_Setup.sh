33904107fde8a8792c6c6fa0539208e0e923960d
W7SVT <w7svt@icloud.com>
2021-10-16 12:29:24 -0700

    Revert "Display comments"
    This reverts commit 2bdd47dc0ed0030f2ec588686d1ca3ea8ef89269.

--- GPSD_NTP_Setup.sh
+++ GPSD_NTP_Setup.sh
@@ -12,38 +12,42 @@
 #########################################################
 
 #*******************************************************
-echo "APT update and upgrade"
+# APT update and upgrade
 #*******************************************************
 apt update && apt upgrade -y
 #*******************************************************
-echo "APT Install GPS PPS and NTP"
+# APT Install GPS PPS and NTP
 #*******************************************************
 apt install gpsd gpsd-clients pps-tools ntp -y
 #*******************************************************
-echo "Alter gpsd"
+# Alter gpsd
 #*******************************************************
 sed -i 's/USBAUTO="true"/USBAUTO="false"/g' /etc/default/gpsd
 sed -i 's:DEVICES="":DEVICES="/dev/serial0 /dev/pps0":g' /etc/default/gpsd
 sed -i 's:GPSD_OPTIONS="":GPSD_OPTIONS="-n":g' /etc/default/gpsd
 #*******************************************************
-echo "Enable PPS and config for GPIO 18"
+# Enable PPS and config for GPIO 18
 #*******************************************************
 echo dtoverlay=pps-gpio,gpiopin=18 >> /boot/config.txt
 echo pps-gpio >> /etc/modules
 #*******************************************************
-echo "Enable services"
+# JIC
+#*******************************************************
+telinit q
+#*******************************************************
+# Enable services
 #*******************************************************
 systemctl enable gpsd
 systemctl enable ntp
 #*******************************************************
-echo "Comment out the internet time sources"
+# Comment out the internet time sources
 #*******************************************************
 sed -i 's/pool 0./# pool 0./g' /etc/ntp.conf
 sed -i 's/pool 1./# pool 1./g' /etc/ntp.conf
 sed -i 's/pool 2./# pool 2./g' /etc/ntp.conf
 sed -i 's/pool 3./# pool 3./g' /etc/ntp.conf
 #*******************************************************
-echo "Confiure /etc/ntp.conf for SHM2 GPS and PPS"
+# Confiure /etc/ntp.conf for SHM2 GPS and PPS
 #*******************************************************
 sudo cat << EOF >> /etc/ntp.conf
 #Note that this config works without an internet source.
